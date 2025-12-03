31130d34c5038882c5f03992cceb27669925322c
Vajravel2008 <vajravel5757@gmail.com>
2025-12-04 01:43:07 +0530

    Support HEAD method in index route
    Added HEAD method support to index route.

--- app.py
+++ app.py
@@ -24,9 +24,12 @@ def process_edf(file_path):
 
     return pred1, pred2
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST", "HEAD"])
 def index():
+    if request.method == "HEAD":
+        return "", 200
     result = ""
+
 
     if request.method == "POST":
         if "file" not in request.files:
